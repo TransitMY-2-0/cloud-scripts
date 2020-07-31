1dac4af92f771f60c1110c6afd1818bbe11a3d8a
Kwong Tung Nan <tungnan5636@gmail.com>
2020-07-31 21:58:03 +0800

    Added uptime webhook feedback to the shutdown script

--- startupMount/shutdownUnmount.sh
+++ startupMount/shutdownUnmount.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 umount -l /mnt/gDrive-Transit
-node /home/kubuntu/scripts/webhooks/simpleServerWebhook.js 0x9C27B0 "server" "SERVER" "Server Shutdown."
+
+myUpTime=$(echo $(uptime))
+node /home/kubuntu/scripts/webhooks/simpleServerWebhook.js 0x9C27B0 "server" "SERVER" "Server Shutdown. $myUpTime"
